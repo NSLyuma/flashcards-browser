9d02261bbf9380e8eba17e9de0ed4281cfba3641
Anastasiya <iinokayo@mail.ru>
2022-12-15 17:50:57 +0300

    Created Layout

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.listen(3000);
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {});
+
+/* eslint-disable no-console */
+app
+  .listen(3000)
+  .on('error', (error) => {
+    console.log("Can't start server");
+    console.log('Error:', error.message);
+  })
+  .on('listening', () => {
+    console.log('Server started successfully');
+  });
+/* eslint-enable no-console */
