97a1da48ceef81ac0a451466b81089e6a24294e0
Anastasiya <iinokayo@mail.ru>
2022-12-15 19:23:36 +0300

    Created db flashcards

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
+require('dotenv').config();
 require('@babel/register');
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const indexRouter = require('./routes/indexRouter');
+const db = require('./db/models');
 
-const app = express();
-
-app.use(morgan('dev'));
 const staticDir = path.join(__dirname, 'public');
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.locals.title = 'Flashcards';
 
+app.use(morgan('dev'));
 // прочесть тело запросов в формате urlencoded -> req.body
 app.use(express.urlencoded({ extended: true }));
 // прочесть тело запросов в формате JSON -> req.body
 app.use(express.json());
 // раздать статические файлы — изображения, стили, клиентские скрипты, etc.
 app.use(express.static(staticDir));
+
 app.use('/', indexRouter);
 
 /* eslint-disable no-console */
 app
-  .listen(3000)
+  .listen(PORT)
   .on('error', (error) => {
     console.log("Can't start server");
     console.log('Error:', error.message);
   })
-  .on('listening', () => {
+  .on('listening', async () => {
     console.log('Server started successfully');
+    try {
+      await db.sequelize.authenticate({ logging: false });
+      console.log('Connected database successfully');
+    } catch (error) {
+      console.log("Can't connect database. ERROR: ", error.message);
+    }
   });
 /* eslint-enable no-console */
