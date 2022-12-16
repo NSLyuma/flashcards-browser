38d06f17b3bfbd15e8e4c0ed5bce1b0466459150
Ibra <Ibrahim-1991@mail.ru>
2022-12-16 16:07:00 +0300

    create TopicPage style

--- views/Deck.jsx
+++ views/Deck.jsx
@@ -3,7 +3,8 @@ const React = require('react');
 module.exports = function Deck({ id, title }) {
   return (
     <li>
-      <a href={`/topicPage/${id}`}>{title}</a>
+      <a href={`/topicPage/${id}`} className="list-group-item list-group-item-action"
+      >{title}</a>
     </li>
   );
 };
