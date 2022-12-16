e392a641d5673588c0d2873f99dd49d5832e8c9d
Anastasiya <iinokayo@mail.ru>
2022-12-16 16:33:50 +0300

    Added styles

--- views/TopicPage.jsx
+++ views/TopicPage.jsx
@@ -1,13 +1,19 @@
-const React = require("react");
-const Deck = require("./Deck");
-const Layout = require("./Layout");
+const React = require('react');
+const Deck = require('./Deck');
+const Layout = require('./Layout');
 
 module.exports = function topicPage({ deck = [] }) {
   return (
     <Layout>
       <div className="list-group">
+        <a
+          href="/"
+          className="list-group-item list-group-item-action active"
+          aria-current="true"
+        >
+          Главная
+        </a>
         <h2
-          href="#"
           className="list-group-item list-group-item-action active"
           aria-current="true"
         >
@@ -15,7 +21,7 @@ module.exports = function topicPage({ deck = [] }) {
         </h2>
       </div>
       <h2 />
-      <ul style={{ listStyleType: "none" }}>
+      <ul style={{ listStyleType: 'none' }}>
         {deck.map((item) => (
           <Deck key={item.id} id={item.id} title={item.title} />
         ))}
